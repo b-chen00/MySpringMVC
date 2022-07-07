67bf9d855810d5ae60e26ab0792f396dde18f7b3
Brandon Chen <bc2982@nyu.edu>
2022-07-07 00:24:29 -0400

    Created home jsp page and entity class. Working on ORM connectivity.

---
+++ src/main/java/com/test/pluto/Customer.java
@@ -0,0 +1,57 @@
+package com.test.pluto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Entity
+@Table(name="customer_record")
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private String email;
+	
+	public Customer() {
+	}
+	
+	public Customer(int i, String n, String e) {
+		super();
+		id = i;
+		name = n;
+		email = e;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int i) {
+		this.id = i;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String n) {
+		this.name = n;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String e) {
+		this.email = e;
+	}
+	
+}
